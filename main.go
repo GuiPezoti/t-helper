a0b9e942f00932e2909f36738b9f7969b01da448
Gui Pezoti <guipezoti@outlook.com>
2025-09-22 23:03:49 -0300

    Reject modernity, return to std library

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
     "fmt"
-    "os"
-
-    tea "github.com/charmbracelet/bubbletea"
-	"github.com/GuiPezoti/t-helper/internal/ui/models"
 )
 
 func main() {
-	model := models.NewAppModel()
-	p := tea.NewProgram(model)
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Erro: %v", err)
-        os.Exit(1)
-    }
+	fmt.Printf("Hello world!")
 }
